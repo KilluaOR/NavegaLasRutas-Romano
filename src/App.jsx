9c79a502a268fae673ccd0fe1a5fcd2fcdd9e9c6
Orne <killu@gmail.com>
2025-09-02 16:54:51 -0300

    adding react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import "./App.css";
+import CartWidget from "./components/CartWidget";
 
 function App() {
   return (
-    <>
-      <NavBar text={"Tabaquería Rodriguez"} bg="rgba(157, 36, 151, 0.83)" />
-      <ItemListContainer x="Bienvenido a Tabaquería Rodriguez" />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/" element={<CartWidget />} />
+        <NavBar text={"Tabaquería Rodriguez"} bg="rgba(157, 36, 151, 0.83)" />
+        <ItemListContainer x="Bienvenido a Tabaquería Rodriguez" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
