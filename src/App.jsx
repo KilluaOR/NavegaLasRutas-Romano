bf9184539a891838b23fd2a1ebf3ecf5e5479ec1
Orne <killu@gmail.com>
2025-09-05 16:58:02 -0300

    trying to use the Routes hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import "./App.css";
@@ -6,15 +7,17 @@ import CartWidget from "./components/CartWidget";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavBar />} />
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/" element={<CartWidget />} />
-        <NavBar text={"Tabaquería Rodriguez"} bg="rgba(157, 36, 151, 0.83)" />
-        <ItemListContainer x="Bienvenido a Tabaquería Rodriguez" />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/" element={<CartWidget />} />
+          <NavBar text={"Tabaquería Rodriguez"} bg="rgba(157, 36, 151, 0.83)" />
+          <ItemListContainer x="Bienvenido a Tabaquería Rodriguez" />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
