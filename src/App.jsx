a80decbe2603d2ab6756beb7f38b9672a9ac90fb
Orne <killu@gmail.com>
2025-09-15 14:10:47 -0300

    cambiando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter>
+      <HashRouter>
         <div className="app">
           <NavBar />
 
@@ -28,7 +28,7 @@ function App() {
             </Routes>
           </main>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </CartProvider>
   );
 }
