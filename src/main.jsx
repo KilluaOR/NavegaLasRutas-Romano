17acf23db001b185029542e487cdb2ad13309633
Orne <killu@gmail.com>
2025-09-15 22:07:50 -0300

    cambios para gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./css/navbar.css";
+import "./css/cartwidget.css";
+import "./css/checkout.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
